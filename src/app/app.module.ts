3deed5fc3fafca1fd86db77d456ddb50d83ee80a
lpoveda <lucio.poveda@gmail.com>
2018-05-26 15:01:22 -0300

    adding basic links to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { BookingService } from './services/booking.service'
+import {HttpClientModule} from '@angular/common/http';
+import {BookingService} from './services/booking.service'
 import {UIRouterModule} from "@uirouter/angular";
 
 import {BidForMealComponent} from './app.component';
-import { NavbarComponent } from './structure/global-navbar/navbar.component';
-import { FooterComponent } from './structure/global-footer/footer.component';
-import { MealsComponent } from './structure/meals/meals.component';
-import { BadgesComponent } from './structure/badges/badges.component';
-import { RewardsComponent } from './structure/rewards/rewards.component';
-import { HelpComponent } from './structure/help/help.component';
+import {NavbarComponent} from './structure/global-navbar/navbar.component';
+import {FooterComponent} from './structure/global-footer/footer.component';
+import {MealsComponent} from './structure/meals/meals.component';
+import {BadgesComponent} from './structure/badges/badges.component';
+import {RewardsComponent} from './structure/rewards/rewards.component';
+import {HelpComponent} from './structure/help/help.component';
 
-let helloState = { name: 'navbar', url: '/navbar',  component: NavbarComponent };
-let aboutState = { name: 'footer', url: '/footer',  component: FooterComponent };
+let meals = {name: 'meals', url: '/meals', component: MealsComponent};
+let badges = {name: 'badges', url: '/badges', component: BadgesComponent};
+let rewards = {name: 'rewards', url: '/rewards', component: RewardsComponent};
+let help = {name: 'help', url: '/help', component: HelpComponent};
 
 @NgModule({
   declarations: [
@@ -28,7 +30,11 @@ let aboutState = { name: 'footer', url: '/footer',  component: FooterComponent }
   imports: [
     BrowserModule,
     HttpClientModule,
-    UIRouterModule.forRoot({ states: [ helloState, aboutState ], useHash: true })
+    UIRouterModule.forRoot({
+        states: [meals, badges, rewards, help],
+        useHash: true
+      }
+    )
   ],
   providers: [BookingService],
   bootstrap: [BidForMealComponent]
