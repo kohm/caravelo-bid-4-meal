a68fc1353efa1012f6704768a2c67e86359ecee4
lpoveda <lucio.poveda@gmail.com>
2018-05-26 07:21:43 -0300

    basic services get implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookingService } from './services/booking.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  booking = {};
+
+  constructor(private _bookingService: BookingService) {}
+
+  ngOnInit() {
+    console.log('asd');
+    this._bookingService.getBooking().subscribe(
+      (resBookingData ) => {
+        this.booking = JSON.stringify(resBookingData);
+        console.log(resBookingData);
+      }
+    )
+  }
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BookingService } from './services/booking.service'
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BookingService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
